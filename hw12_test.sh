9a90ccaa79743d1485834c2f55564f2f74a9ac7a
TsengMJ <tsngmj@gmail.com>
2020-06-10 14:03:12 +0800

    HW 12

---
+++ hw12_test.sh
@@ -0,0 +1,4 @@
+wget https://github.com/NTU-speech-lab/hw12-TsengMJ/releases/download/1/extractor_model_5000.bin
+wget https://github.com/NTU-speech-lab/hw12-TsengMJ/releases/download/0/predictor_model_5000.bin
+
+python ./Src/test.py $1 $2
